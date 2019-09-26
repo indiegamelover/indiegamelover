f5d9839c0e09724043f53ccd44f704293de3f529
indiegamelover <indiegamelover@hotmail.com>
2019-09-25 18:13:53 -0700

    Update youtube.js
    test

--- youtube.js
+++ youtube.js
@@ -1,26 +1,29 @@
-<style type='text/css'>
-		p {margin-bottom:2em;}
-		iframe,.ytLoader {display:block;margin:0 auto;width:50vw;height:37.5vw;border:0.5vw solid #ccc;}
-		.ytLoader {position:relative;cursor:pointer;overflow:hidden;}
-		.ytLoader>.cover {width:100%;height:auto;}
-		.ytLoader>.playBtn {position:absolute;top:50%;left:50%;width:20%;height:auto;transform:translate(-50%,-50%);opacity:0.5;}
-</style>
+( function() {
 
-		function youTubes_makeDynamic() {
-			var $ytIframes = $('iframe[src*="youtube.com"]');
-			$ytIframes.each(function (i,e) {
-				var $ytFrame = $(e);
-				var ytKey; var tmp = $ytFrame.attr('src').split(/\//); tmp = tmp[tmp.length - 1]; tmp = tmp.split('?'); ytKey = tmp[0];
-				var $ytLoader = $('<div class="ytLoader">');
-				$ytLoader.append($('<img class="cover" src="https://i.ytimg.com/vi/'+ytKey+'/hqdefault.jpg">'));
-				$ytLoader.append($('<img class="playBtn" src="https://3.bp.blogspot.com/-OI0nhlpimc0/XYtTtF7UWjI/AAAAAAAAHeQ/Rscxb4wXLVU6iun9mT1Jpc8Tpa4zHo43wCLcBGAsYHQ/s1600/PlayButton100.png">'));
-				$ytLoader.data('$ytFrame',$ytFrame);
-				$ytFrame.replaceWith($ytLoader);
-				$ytLoader.click(function () {
-					var $ytFrame = $ytLoader.data('$ytFrame');
-					$ytFrame.attr('src',$ytFrame.attr('src')+'?autoplay=1');
-					$ytLoader.replaceWith($ytFrame);
-				});
-			});
-		};
-		$(document).ready(function () {youTubes_makeDynamic()});
+    var youtube = document.querySelectorAll( ".youtube" );
+    
+    for (var i = 0; i < youtube.length; i++) {
+        
+        var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/hqdefault.jpg";
+        
+        var image = new Image();
+                image.src = source;
+                image.addEventListener( "load", function() {
+                    youtube[ i ].appendChild( image );
+                }( i ) );
+        
+                youtube[i].addEventListener( "click", function() {
+
+                    var iframe = document.createElement( "iframe" );
+
+                            iframe.setAttribute( "frameborder", "0" );
+                            iframe.setAttribute( "allowfullscreen", "" );
+                            iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+
+                            this.innerHTML = "";
+                            this.appendChild( iframe );
+                } );    
+    };
+    
+} )();
+
